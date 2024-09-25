262798375ebdb08451076842937ea57f0eb25337
Giovanni Nespoli <ggnespoli@gmail.com>
2024-09-25 11:12:05 -0300

    authentication integration

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,33 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UserRepository } from './users.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { JwtPayload } from './jwt-payload.interface';
+import { User } from './user.entity';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(UserRepository)
+    private usersRepository: UserRepository,
+  ) {
+    super({
+      secretOrKey: 'topSecret51',
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken,
+    });
+  }
+
+  async validate(payload: JwtPayload) {
+    const { username } = payload;
+    const user: User = await this.usersRepository.findOne({
+      where: { username },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    return user;
+  }
+}
